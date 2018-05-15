e31b9643f75d894efa5e5d0139ccbb0335b6f96d
Tim Buckland <tim.buckland@totaljobsgroup.com>
2018-05-15 21:00:52 +0100

    added checkNotDisplayed, keys and random string generation

--- Froth.java
+++ Froth.java
@@ -1,14 +1,29 @@
+package qarj.pointyarrow;
+
 import android.support.test.espresso.AmbiguousViewMatcherException;
+import android.support.test.espresso.NoMatchingViewException;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import java.security.SecureRandom;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.IsNot.not;
 
 public class Froth {
 
@@ -39,4 +54,59 @@ public class Froth {
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
     }
 
+    public static void checkNotDisplayed(String notExpected) {
+        try {
+            onView(withText(containsString(notExpected))).check(matches(not(isDisplayed())));
+        } catch (NoMatchingViewException e) {
+            // this is a pass too
+        } catch (AmbiguousViewMatcherException e) {
+            assertTrue("checkNotDisplayed - string found more than once!",false);
+        }
+    }
+
+    public static void keys(String anchor, String text) {
+        // Currently just supports hint
+        onView(allOf(withHint(anchor), isDisplayed()))
+                .perform(typeText(text), closeSoftKeyboard());
+
+        //https://stackoverflow.com/questions/29378552/in-espresso-how-to-choose-one-the-view-who-have-same-id-to-avoid-ambiguousviewm
+        //Needs more work to figure this out:
+        //        onView(withIndex(allOf(withHint(anchor), isDisplayed()), 0))
+        //                .perform(typeText(text), closeSoftKeyboard());
+    }
+
+    public static String random() {
+       return randomAlpha(5);
+    }
+
+    static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static SecureRandom rnd = new SecureRandom();
+
+    static String randomAlpha(int len ){
+        StringBuilder sb = new StringBuilder( len );
+        for( int i = 0; i < len; i++ )
+            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+        return sb.toString();
+    }
+
+    //https://stackoverflow.com/questions/29378552/in-espresso-how-to-choose-one-the-view-who-have-same-id-to-avoid-ambiguousviewm
+    //FrostRocket
+    public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
+        return new TypeSafeMatcher<View>() {
+            int currentIndex = 0;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with index: ");
+                description.appendValue(index);
+                description.appendText(" ");
+                matcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                return matcher.matches(view) && currentIndex++ == index;
+            }
+        };
+    }
 }
